fc8fcb4cbc36afdb3a49a817a0f6a3887d56ff29
Anjiboss <nhu.nhu.face@gmail.com>
2021-04-03 09:03:34 +0900

    add short error handler

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("You are On Home");
-})
+});
 
-app.listen(PORT,() => console.log("Running on "+ PORT))
+const server = app.listen(PORT, () => console.log("Running on " + PORT));
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Logged Error: ${err}`);
+  server.close(process.exit(1));
+});
