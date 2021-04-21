975322c01905a71e62f98613efda3d9aa1a92537
Anjiboss <nhu.nhu.face@gmail.com>
2021-04-21 13:12:35 +0900

    server send image

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const path = require("path");
 
 const PORT = process.env.PORT || 5000;
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("You are On Home");
+});
+
+app.get("/api/img", (req, res) => {
+  res.sendFile(path.join(__dirname, "image", "portfolioIMG.png"));
 });
 
 const server = app.listen(PORT, () => console.log("Running on " + PORT));
