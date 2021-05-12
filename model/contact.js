ee17a794af85ce559f97e3c9cdd7bfc304f94da2
Anjiboss <nhu.nhu.face@gmail.com>
2021-05-12 11:58:00 +0900

    remove mongoose

--- model/contact.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const contactSchema = new mongoose.Schema({
-  name: String,
-  text: String,
-});
-
-module.exports = mongoose.model("Contact", contactSchema);
--- server.js
+++ server.js
@@ -3,16 +3,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const path = require("path");
-const connectDB = require("./config/db");
-const contact = require("./model/contact");
 const sendMail = require("./utils/sendMail");
 let contactCount = 0;
 
 const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-
-connectDB();
 
 app.get("/", (req, res) => {
   res.send("You not suppose to request like this ;>");
@@ -23,18 +19,7 @@ app.get("/kidoshitekure", (req, res) => {
 });
 
 app.post("/api/contact", async (req, res) => {
-  // const _contact = new contact({
-  //   name: req.body.name,
-  //   text: req.body.text,
-  // });
-  // _contact.save((err) => {
-  //   if (err) {
-  //     console.error(err);
-  //   }
-  // });
   contactCount++;
-  console.log(contactCount);
-
   sendMail(req.body.name, req.body.text, () => {
     res.status(200).send("done");
   });
